a3c0b38c7af83b9098f6639e04e9677fccef37a6
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-22 02:15:12 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,13 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer 
-  include Dance 
+class Dancer
+  include Dance
 
-  attr_accessor :name 
+  attr_accessor :name
 
   def initialize(name)
-    @name = name 
-  end 
-  
+    @name = name
+  end
+
 end 
